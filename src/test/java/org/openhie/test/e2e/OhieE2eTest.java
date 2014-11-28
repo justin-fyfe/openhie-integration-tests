742998850d5f4a5e8f49360119f1eb992ab3a1d7
Ryan Crichton <rg.crichton@gmail.com>
2014-11-28 11:29:10 +0200

    Added a test for multiple docuemnts in a submission set. Added IHE certs. Updated readme to describe TLS testing.

--- src/test/java/org/openhie/test/e2e/OhieE2eTest.java
+++ src/test/java/org/openhie/test/e2e/OhieE2eTest.java
@@ -40,7 +40,7 @@ public class OhieE2eTest {
 	 */
 	@Test
 	public void testSuccessfulProvideAndRetrieve() throws HL7Exception, IOException, LLPException, JAXBException {
-		
+
 		// STEP 10 & 20 : Run e2e on PIX
 		Message step10 = CrMessageUtil.loadMessage("OHIE-CR-11-10"),
 				step20 = CrMessageUtil.loadMessage("OHIE-CR-11-20");
@@ -56,7 +56,7 @@ public class OhieE2eTest {
 		CrMessageUtil.assertAccepted(assertTerser);
 		CrMessageUtil.assertHasOneQueryResult(assertTerser);
 		CrMessageUtil.assertHasPID3Containing(assertTerser.getSegment("/QUERY_RESPONSE(0)/PID"), "RJ-439", "TEST", OhieCrIntegrationTest.TEST_DOMAIN_OID);
-		
+
 		// STEP 30 - Load the XDS PnR
 		
 		String documentId = String.format("1.3.6.1.4.1.21367.2010.1.2.%s", new SimpleDateFormat("HHmmss").format(new Date()));
@@ -79,5 +79,54 @@ public class OhieE2eTest {
 		assertEquals(1, retrieveResponse.getDocumentResponse().size());
 		XdsMessageUtil.assertMatchesHash(queryResponse, retrieveResponse);
 	}
+
+    /**
+     * Test basic case with multiple documents
+     * @throws IOException
+     * @throws HL7Exception
+     * @throws LLPException
+     * @throws JAXBException
+     */
+    @Test
+    public void testSuccessfulProvideAndRetrieveWithMultipleDocuments() throws HL7Exception, IOException, LLPException, JAXBException {
+
+        // STEP 10 & 20 : Run e2e on PIX
+        Message step10 = CrMessageUtil.loadMessage("OHIE-CR-11-10"),
+                step20 = CrMessageUtil.loadMessage("OHIE-CR-11-20");
+
+        // STEP 10 - Send to PIX
+        Message response = CrMessageUtil.sendMessage(step10);
+        Terser assertTerser = new Terser(response);
+        CrMessageUtil.assertAccepted(assertTerser);
+
+        // STEP 20 - Query PIX to verify creation
+        response = CrMessageUtil.sendMessage(step20);
+        assertTerser = new Terser(response);
+        CrMessageUtil.assertAccepted(assertTerser);
+        CrMessageUtil.assertHasOneQueryResult(assertTerser);
+        CrMessageUtil.assertHasPID3Containing(assertTerser.getSegment("/QUERY_RESPONSE(0)/PID"), "RJ-439", "TEST", OhieCrIntegrationTest.TEST_DOMAIN_OID);
+
+        // STEP 30 - Load the XDS PnR
+
+        String documentId = String.format("1.3.6.1.4.1.21367.2010.1.2.%s", new SimpleDateFormat("HHmmss").format(new Date()));
+        ProvideAndRegisterDocumentSetRequestType pnrRequest = XdsMessageUtil.loadMessage("OHIE-XDS-01-10-multi", ProvideAndRegisterDocumentSetRequestType.class);
+        XdsMessageUtil.updatePnrPatientInfo(pnrRequest, assertTerser.getSegment("/QUERY_RESPONSE(0)/PID"), documentId);
+        RegistryResponseType xdsResponse = XdsMessageUtil.provideAndRegister(pnrRequest);
+        XdsMessageUtil.assertSuccess(xdsResponse);
+
+        // STEP 40 - Load the XDS query and perform a query
+
+        AdhocQueryRequest queryRequest = XdsMessageUtil.loadMessage("OHIE-XDS-01-20", AdhocQueryRequest.class);
+        XdsMessageUtil.updateAdhocQueryRequest(queryRequest, assertTerser.getSegment("/QUERY_RESPONSE(0)/PID"));
+        AdhocQueryResponse queryResponse = XdsMessageUtil.registryStoredQuery(queryRequest);
+        XdsMessageUtil.assertSuccess(queryResponse);
+        XdsMessageUtil.assertHasDocumentId(queryResponse, documentId);
+
+        // STEP 50 - Retrieve
+        RetrieveDocumentSetRequestType retrieveRequest =  XdsMessageUtil.createRetrieveRequest(documentId, queryResponse);
+        RetrieveDocumentSetResponseType retrieveResponse = XdsMessageUtil.retrieveDocumentSet(retrieveRequest);
+        assertEquals(1, retrieveResponse.getDocumentResponse().size());
+        XdsMessageUtil.assertMatchesHash(queryResponse, retrieveResponse);
+    }
 
 }
